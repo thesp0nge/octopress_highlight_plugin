472a068677406efe84e85f821f815e18345d79ab
Paolo Perego <thesp0nge@gmail.com>
2012-08-28 08:48:39 +0200

    First import

---
+++ highlight.rb
@@ -0,0 +1,29 @@
+# This is the Octopress Gravatar plugin
+#
+# Place this file in your plugin directory and you have an _highlight_ keyword to create a span with highlight class containing your text
+#
+# Usage: {% gravatar email@domain.com %}
+#
+# Paolo Perego, <thesp0nge@gmail.com>, v20120828.a
+# repo@github: https://github.com/thesp0nge/octopress_gravatar_plugin
+# twitter: @thesp0nge
+# blog: http://armoredcode.com
+#
+
+module Jekyll
+  class Highlight < Liquid::Tag
+    @text = ""
+
+    def initialize(tagname, text, tokens)
+      @text = text
+    end
+
+    def render(context)
+      "<span class=\"fluo\">#{@text}</span>"
+    end
+
+  end
+
+
+end
+Liquid::Template.register_tag('highlight', Jekyll::Highlight)
